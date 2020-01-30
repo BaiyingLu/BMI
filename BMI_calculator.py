ceaaf9f3d97a96bfa2e597829016b6a3d4867871
Baiying Lu <bl223@duke.edu>
2020-01-29 21:37:56 -0500

    Design the unit selection and value input part

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,4 +1,26 @@
 # To calculate the BMI of a person
+def unit_select_and_input():
+    while True:
+        print("Please input the calculation unit")
+        print("Please input the weight unit first")
+        print("Input 'kg(kilograms)' or 'lbs(pounds)'")
+        weight_unit = input("Input the weight unit:")
+        print("The weight unit you input is {}".format(weight_unit))
+        print("Please input the weight")
+        weight = input("Input the weight:")
+
+
+        print("Next, please input the height unit")
+        print("Input 'm(meters)' or 'inches' or 'cm'")
+        height_unit = input("Input the height unit:")
+        print("The height unit you input is {}".format(height_unit))
+        print("Please input the weight")
+        height = input("Input the height:")
+
+        return weight_unit,weight,height_unit,height
+
+
+
 def interface():
     while True:
         print("This is a BMI calculator")
@@ -9,7 +31,8 @@ def interface():
         if choice == '9':
             return
         elif choice == "1":
-            pass
+            weight_unit,weight,height_unit,height = unit_select_and_input()
+            print(weight_unit,weight,height_unit,height)
 
 
 if __name__=="__main__":
